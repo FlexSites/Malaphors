8c941cf0a71f307d3378d697fbde45d97097166b
Travis Duncombe <travisduncombe18@gmail.com>
2017-02-09 21:49:28 -0700

    Lint the things. Or just disable rules you don’t like, either way.

--- services/idiom.js
+++ services/idiom.js
@@ -7,25 +7,24 @@ const shuffle = require('lodash.shuffle');
 const pluralize = require('pluralize');
 const upperCase = require('upper-case-first');
 
-const VOWELS = ['a', 'e', 'i', 'o', 'u'];
+const VOWELS = [ 'a', 'e', 'i', 'o', 'u' ];
 const NOUN_TOKEN = 'noun';
 const PLURAL_NOUN_TOKEN = 'nouns';
 const ADJECTIVE_TOKEN = 'adj';
 
 function randomIdiom() {
-  const idioms = getRandomElements(Idioms, 2);
+  const idioms = getRandomElements(Idioms, 2);//eslint-disable-line no-magic-numbers
 
-  console.log('MERGE', idioms.map((idiom) => idiom.idiom));
+  console.log('MERGE', idioms.map((idiom) => idiom.idiom));//eslint-disable-line
 
-  return upperCase(mergeIdioms(...idioms));
+  return upperCase(mergeIdioms(...idioms));//eslint-disable-line node/no-unsupported-features
 }
 
 function half() {
-  return Math.random() > 0.5;
+  return Math.random() > 0.5;//eslint-disable-line no-magic-numbers
 }
 
 function mergeIdioms(entry1, entry2) {
-
   const idiom1 = getRandomElement(entry1.replacements);
   const idiom2 = getRandomElement(entry2.replacements);
   const maxPartLen = Math.max(idiom1.length, idiom2.length);
@@ -67,7 +66,7 @@ function fixArticles(str) {
 
   if (matches) {
     matches.forEach((match) => {
-      const lastChar = match.substr(match.length - 1).toLowerCase();
+      const lastChar = match.substr(match.length - 1).toLowerCase();//eslint-disable-line no-magic-numbers
 
       if (VOWELS.includes(lastChar)) {
         str = str.replace(match, ` an ${ lastChar }`);
@@ -79,15 +78,16 @@ function fixArticles(str) {
 }
 
 function getRandomElements(arr, num) {
-  return shuffle(arr).slice(0, Math.min(num, arr.length));
+  return shuffle(arr).slice(0, Math.min(num, arr.length));//eslint-disable-line no-magic-numbers
 }
 
 function getRandomElement(arr, remove) {
   const idx = Math.floor(Math.random() * arr.length);
 
   if (remove) {
-    return arr.splice(idx, 1)[0];
+    return arr.splice(idx, 1)[0];//eslint-disable-line no-magic-numbers
   }
+
   return arr[idx];
 }
 
