0de3f4b62a1b3fd91a1505305627c1f9eefa7e85
Seth Tippetts <sethtippetts@gmail.com>
2017-02-23 23:41:52 -0700

    Switching screenshot packages

--- services/screenshot.js
+++ services/screenshot.js
@@ -1,26 +1,8 @@
 'use strict'
 
-const webshot = require('webshot')
-const path = require('path')
+const screenshot = require('screenshot-stream')
 
-exports.default = screenshot
-
-function screenshot (id, width = 1024, height = 768, res) {
-  return webshot(`http://localhost:3000/${ id }`, {
-    screenSize: {
-      width,
-      height,
-    },
-    streamType: 'jpg',
-    phantomPath: path.join(__dirname, '../vendor/phantomjs/bin/phantomjs'),
-  }, (err, stream) => {
-    if (err) {
-      console.error(err)
-      res.status(500).send({
-        message: err.message || 'PhantomJS failed to render',
-      })
-    }
-    stream.pipe(res)
-  })
-    // .pipe(fs.createWriteStream('myscreenshot.jpg'))
+exports.default = (id, width = 1024, height = 768, res) => {
+  return screenshot(`https://idioms-today.herokuapp.com/${ id }`, `${ width }x${ height }`)
+    .pipe(res)
 }
