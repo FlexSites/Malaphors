0760b53475b92a4a38831b0029a08bea35a3599b
Seth Tippetts <sethtippetts@gmail.com>
2017-02-25 00:25:20 -0700

    Remove S3 prefix so locally it's local and prod it's cloud front Remove semicolons from Idiom service

--- services/idiom.js
+++ services/idiom.js
@@ -1,105 +1,105 @@
-'use strict';
+'use strict'
 
-const Idioms = require('../idioms.json');
-const Authors = require('../authors.json');
-const Images = require('../images.json');
-const shuffle = require('lodash.shuffle');
-const pluralize = require('pluralize');
-const upperCase = require('upper-case-first');
+const Idioms = require('../idioms.json')
+const Authors = require('../authors.json')
+const Images = require('../images.json')
+const shuffle = require('lodash.shuffle')
+const pluralize = require('pluralize')
+const upperCase = require('upper-case-first')
 
-const VOWELS = [ 'a', 'e', 'i', 'o', 'u' ];
-const NOUN_TOKEN = 'noun';
-const PLURAL_NOUN_TOKEN = 'nouns';
-const ADJECTIVE_TOKEN = 'adj';
+const VOWELS = [ 'a', 'e', 'i', 'o', 'u' ]
+const NOUN_TOKEN = 'noun'
+const PLURAL_NOUN_TOKEN = 'nouns'
+const ADJECTIVE_TOKEN = 'adj'
 
 function randomIdiom() {
-  const idioms = getRandomElements(Idioms, 2);//eslint-disable-line no-magic-numbers
+  const idioms = getRandomElements(Idioms, 2)//eslint-disable-line no-magic-numbers
 
-  console.log('MERGE', idioms.map((idiom) => idiom.idiom));//eslint-disable-line
+  console.log('MERGE', idioms.map((idiom) => idiom.idiom))//eslint-disable-line
 
-  return upperCase(mergeIdioms(...idioms));//eslint-disable-line node/no-unsupported-features
+  return upperCase(mergeIdioms(...idioms))//eslint-disable-line node/no-unsupported-features
 }
 
 function half() {
-  return Math.random() > 0.5;//eslint-disable-line no-magic-numbers
+  return Math.random() > 0.5//eslint-disable-line no-magic-numbers
 }
 
 function mergeIdioms(entry1, entry2) {
-  const idiom1 = getRandomElement(entry1.replacements);
-  const idiom2 = getRandomElement(entry2.replacements);
-  const maxPartLen = Math.max(idiom1.length, idiom2.length);
-  const nouns = [].concat(entry1.nouns, entry2.nouns);
-  const adjectives = [].concat(entry1.adjectives || [], entry2.adjectives || []);
-  const parts = [];
+  const idiom1 = getRandomElement(entry1.replacements)
+  const idiom2 = getRandomElement(entry2.replacements)
+  const maxPartLen = Math.max(idiom1.length, idiom2.length)
+  const nouns = [].concat(entry1.nouns, entry2.nouns)
+  const adjectives = [].concat(entry1.adjectives || [], entry2.adjectives || [])
+  const parts = []
 
-  for (let i = 0; i < maxPartLen; i++) {
-    const part = half() ? idiom1[i] || idiom2[i] : idiom2[i] || idiom1[i];
+  for (let i = 0 i < maxPartLen i++) {
+    const part = half() ? idiom1[i] || idiom2[i] : idiom2[i] || idiom1[i]
 
-    parts.push(part);
+    parts.push(part)
   }
 
-  let str = parts.join(' ');
+  let str = parts.join(' ')
 
   str = str.replace(/\{\{ (adj|nouns?) \}\}/g, (match, p1) => {
     if (p1 === NOUN_TOKEN) {
-      const noun = getRandomElement(nouns, true);
+      const noun = getRandomElement(nouns, true)
 
-      return pluralize.singular(noun);
+      return pluralize.singular(noun)
     } else if (p1 === PLURAL_NOUN_TOKEN) {
-      const noun = getRandomElement(nouns, true);
+      const noun = getRandomElement(nouns, true)
 
-      return pluralize.plural(noun);
+      return pluralize.plural(noun)
     } else if (p1 === ADJECTIVE_TOKEN) {
-      const adj = getRandomElement(adjectives, true);
+      const adj = getRandomElement(adjectives, true)
 
-      return adj;
+      return adj
     }
 
-    return '';
-  });
+    return ''
+  })
 
-  return fixArticles(str);
+  return fixArticles(str)
 }
 
 function fixArticles(str) {
-  const matches = str.match(/ an? [a-z]/gi);
+  const matches = str.match(/ an? [a-z]/gi)
 
   if (matches) {
     matches.forEach((match) => {
-      const lastChar = match.substr(match.length - 1).toLowerCase();//eslint-disable-line no-magic-numbers
+      const lastChar = match.substr(match.length - 1).toLowerCase()//eslint-disable-line no-magic-numbers
 
       if (VOWELS.includes(lastChar)) {
-        str = str.replace(match, ` an ${ lastChar }`);
+        str = str.replace(match, ` an ${ lastChar }`)
       }
-    });
+    })
   }
 
-  return str;
+  return str
 }
 
 function getRandomElements(arr, num) {
-  return shuffle(arr).slice(0, Math.min(num, arr.length));//eslint-disable-line no-magic-numbers
+  return shuffle(arr).slice(0, Math.min(num, arr.length))//eslint-disable-line no-magic-numbers
 }
 
 function getRandomElement(arr, remove) {
-  const idx = Math.floor(Math.random() * arr.length);
+  const idx = Math.floor(Math.random() * arr.length)
 
   if (remove) {
-    return arr.splice(idx, 1)[0];//eslint-disable-line no-magic-numbers
+    return arr.splice(idx, 1)[0]//eslint-disable-line no-magic-numbers
   }
 
-  return arr[idx];
+  return arr[idx]
 }
 
 function randomAuthor() {
-  return getRandomElement(Authors);
+  return getRandomElement(Authors)
 }
 
 function randomImage() {
-  return getRandomElement(Images);
+  return getRandomElement(Images)
 }
 
-exports.randomImage = randomImage;
-exports.randomAuthor = randomAuthor;
-exports.randomIdiom = randomIdiom;
+exports.randomImage = randomImage
+exports.randomAuthor = randomAuthor
+exports.randomIdiom = randomIdiom
 
