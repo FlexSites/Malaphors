e76eddee063f323a66a9ebb33c315c062236a20d
Seth Tippetts <sethtippetts@gmail.com>
2017-05-07 04:22:42 -0600

    Vastly improve algorithm and idiom content accuracy

--- services/idiom.js
+++ services/idiom.js
@@ -7,43 +7,33 @@ const Images = require('../assets/images.json')
 const shuffle = require('lodash.shuffle')
 const pluralize = require('pluralize')
 const upperCase = require('upper-case-first')
+const VERBS = require('../assets/verbs.json')
 
 const VOWELS = [ 'a', 'e', 'i', 'o', 'u' ]
-const NOUN_TOKEN = 'noun'
-const PLURAL_NOUN_TOKEN = 'nouns'
-const VERB_TOKEN = 'verb'
-const ADJECTIVE_TOKEN = 'adj'
 
 const ALL_VERBS = Idioms.reduce((prev, curr) => {
   return prev.concat(curr.verbs || []);
 }, [])
 
-console.log(ALL_VERBS);
+console.log(ALL_VERBS)
+
+console.log(Object.keys(VERBS))
+
 
 function randomIdiom () {
-  const idioms = getRandomElements(Idioms, 2)//eslint-disable-line no-magic-numbers
+  const idioms = getRandomElements(Idioms, 3)//eslint-disable-line no-magic-numbers
 
-  console.log('MERGE', idioms.map((idiom) => idiom.idiom))//eslint-disable-line
-
-  return upperCase(mergeIdioms(...idioms))//eslint-disable-line node/no-unsupported-features
-}
-
-function half() {
-  return Math.random() > 0.5//eslint-disable-line no-magic-numbers
-}
-
-function mergeIdioms(entry1, entry2) {
+  console.log('MERGING\n\n', idioms.map(({ idiom }) => idiom).join(' + \n'));
   const merge = upperCase(mergeIdioms(...idioms)) // eslint-disable-line node/no-unsupported-features
   const cleaned = clean(merge)
 
   if (dupHash.includes(cleaned)) {
-    console.log('HAD TO REDO, DUPLICATE')
     return randomIdiom()
   }
   return merge
 }
 
-function clean(text) {
+function clean (text) {
   return text.toLowerCase().replace(/[^a-z]/g, '');
 }
 
@@ -51,13 +41,11 @@ function half () {
   return Math.random() > 0.5//eslint-disable-line no-magic-numbers
 }
 
-function mergeIdioms (entry1, entry2) {
+function mergeIdioms (...entries) {
+  const [ entry1, entry2 ] = entries
   const idiom1 = getRandomElement(entry1.replacements)
   const idiom2 = getRandomElement(entry2.replacements)
   const maxPartLen = Math.max(idiom1.length, idiom2.length)
-  const nouns = [].concat(entry1.nouns, entry2.nouns)
-  const verbs = [].concat(entry1.verbs || [], entry2.verbs || [])
-  const adjectives = [].concat(entry1.adjectives || [], entry2.adjectives || [])
   const parts = []
 
   for (let i = 0; i < maxPartLen; i++) {
@@ -68,28 +56,9 @@ function mergeIdioms (entry1, entry2) {
 
   let str = parts.join(' ')
 
-  str = str.replace(/\{\{ (adj|nouns?|verbs?) \}\}/g, (match, p1) => {
-    if (p1 === NOUN_TOKEN) {
-      const noun = getRandomElement(nouns, true)
-    if (p1 === NOUN_TOKEN) {
-      const noun = getRandomElement(nouns, true)
-      return pluralize.singular(noun)
-    } else if (p1 === PLURAL_NOUN_TOKEN) {
-      const noun = getRandomElement(nouns, true)
+  const malaphor = new Malaphor(...entries)
 
-      return pluralize.plural(noun)
-    } else if (p1 === ADJECTIVE_TOKEN) {
-      const adj = getRandomElement(adjectives, true)
-
-      return adj
-    } else if (p1 === VERB_TOKEN) {
-      return getRandomElement(verbs, true)
-    }
-
-    return ''
-  })
-
-  return fixArticles(str)
+  return fixArticles(malaphor.parse(str))
 }
 
 function fixArticles (str) {
@@ -112,11 +81,15 @@ function getRandomElements (arr, num) {
   return shuffle(arr).slice(0, Math.min(num, arr.length))//eslint-disable-line no-magic-numbers
 }
 
-function getRandomElement (arr, remove) {
+function getRandomElement (arr = [], remove = false) {
   const idx = Math.floor(Math.random() * arr.length)
 
+  if (!arr.length) {
+    return null
+  }
+
   if (remove) {
-    return arr.splice(idx, 1)[0]//eslint-disable-line no-magic-numbers
+    return arr.splice(idx, 1)[0] // eslint-disable-line no-magic-numbers
   }
 
   return arr[idx]
@@ -134,3 +107,84 @@ exports.randomImage = randomImage
 exports.randomAuthor = randomAuthor
 exports.randomIdiom = randomIdiom
 exports.getRandomElement = getRandomElement;
+
+
+exports.getRandomElement = getRandomElement
+
+
+export default class Malaphor {
+  constructor (...sources) {
+    this.sources = sources
+
+    this.nouns = []
+    this.verbs = []
+    this.adjectives = []
+    this.abstract_nouns = []
+    this.mass_nouns = []
+    this.proper_nouns = []
+
+    this.sources.reduce((prev, curr) => {
+      prev.nouns = prev.nouns.concat(curr.nouns || [])
+      prev.abstract_nouns = prev.abstract_nouns.concat(curr.abstract_nouns || [])
+      prev.mass_nouns = prev.mass_nouns.concat(curr.mass_nouns || [])
+      prev.proper_nouns = prev.proper_nouns.concat(curr.proper_nouns || [])
+      prev.verbs = prev.verbs.concat(curr.verbs || [])
+      prev.adjectives = prev.adjectives.concat(curr.adjectives || [])
+      return prev
+    }, this)
+  }
+
+  abstract (type) {
+    return getRandomElement(this.abstract_nouns, true)
+  }
+  mass (type) {
+    const potential = this.mass_nouns.concat(this.nouns);
+    return getRandomElement(potential, true)
+  }
+  proper (type) {
+    console.log('proper_nouns', type)
+    return getRandomElement(this.proper_nouns, true) || getRandomElement(this.nouns, true)
+  }
+
+  noun (type) {
+    const noun = getRandomElement(this.nouns, true)
+
+    if (type === 'plural') {
+      return pluralize.plural(noun)
+    }
+
+    return noun
+  }
+
+  adjective (type) {
+    return getRandomElement(this.adjectives, true)
+  }
+
+  verb (type) {
+    const verb = getRandomElement(this.verbs, true)
+    const pos = VERBS[verb]
+
+    if (pos) {
+      return pos[type]
+    }
+
+    if (type === 'present') {
+      return verb
+    } else if (type === 'continuous') {
+      return `${ verb }ing`
+    } else if (type === 'past') {
+      return `${ verb }ed`
+    } else if (type === 'future') {
+      return `will ${ verb }`
+    }
+  }
+
+  parse (str) {
+    return str.replace(/\{\{ (.*?) \}\}/g, (match, p1) => {
+      const [ pos, type ] = p1.split('_')
+
+
+      return this[pos](type)
+    })
+  }
+}
